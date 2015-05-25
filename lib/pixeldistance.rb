00f44b1937d7b657688694a61058604d64a8a2f9
Bruno Salerno <br.salerno@gmail.com>
2015-05-25 12:08:14 -0300

    module_functions

--- lib/pixeldistance.rb
+++ lib/pixeldistance.rb
@@ -5,7 +5,7 @@ module PixelDistance
   RADIUS = 85445659.4471
   ZOOM = 12
 
-  def self.from_coords(lat1,lon1,lat2,lon2,zoom = nil)
+  def from_coords(lat1,lon1,lat2,lon2,zoom = nil)
     zoom ||= ZOOM
 
     x1 = lon_to_x(lon1)
@@ -16,14 +16,15 @@ module PixelDistance
     Math.sqrt((x1-x2)**2 + (y1-y2)**2).to_i >> (21 - zoom)
   end
 
-  private
-  def self.lon_to_x(lon)
+  def lon_to_x(lon)
     (OFFSET + RADIUS * lon * Math::PI / 180).to_i
   end
 
-  def self.lat_to_y(lat)
+  def lat_to_y(lat)
     (OFFSET + RADIUS *
         Math.log((1 + Math.sin(lat * Math::PI / 180)) /
                      (1 - Math.sin(lat * Math::PI / 180))) / 2)
   end
+
+  module_function :from_coords,:lon_to_x,:lat_to_y
 end
